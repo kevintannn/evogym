a383124c8a01e48bdb566bbe6c0f8a50f47a88a8
Kevin Tan <48116781+kevintannn@users.noreply.github.com>
2024-01-09 13:06:40 +0700

    increase z-index of navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,7 +22,7 @@ const Navbar = ({ selectedPage, setSelectedPage, isTopOfPage }: Props) => {
   return (
     <nav>
       <div
-        className={`${navbarBackground} ${flexBetween} fixed top-0 z-30 w-full py-6`}
+        className={`${navbarBackground} ${flexBetween} fixed top-0 z-50 w-full py-6`}
       >
         <div className={`${flexBetween} mx-auto w-5/6 gap-16`}>
           {/* LEFT SIDE */}
